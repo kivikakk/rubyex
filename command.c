28a09bfa829c2867d37d6d2bed865b81d877f51d
Arlen Christian Mart Cuss <celtic@vm>
2008-02-02 13:36:24 +0000

    Getting ready for cell.remove. Ignoring .*.d.

--- command.c
+++ command.c
@@ -6,6 +6,7 @@
 char *get_next_nonblank_token(char **);
 void cell_new(char **);
 void cell_list(char **);
+void cell_remove(char **);
 
 void evaluate_command(const char *command)
 {
@@ -24,6 +25,8 @@ void evaluate_command(const char *command)
     cell_new(&ssptr);
   else if (strcmp(token, "cell.list") == 0)
     cell_list(&ssptr);
+  else if (strcmp(token, "cell.remove") == 0)
+    cell_remove(&ssptr);
     
   vfree(our_command);
 }
@@ -64,6 +67,34 @@ void cell_list(char **ssptr)
     it = it->next;
   }
   printk(KERN_INFO "rubyex: cell.list ends\n");
+}
+
+void cell_remove(char **ssptr)
+{
+  struct cell_list_node *it;
+  struct cell *cell;
+  char *token = get_next_nonblank_token(ssptr);
+
+  if (!token) {
+    printk(KERN_WARNING "rubyex: cell.remove: requires argument (the name of the cell to remove)\n");
+    return;
+  }
+
+  it = rubyex_cells->head;
+  while (it) {
+    if (strcmp(it->cell->name, token) == 0) {
+      cell = it->cell;
+      break;
+    }
+    it = it->next;
+  }
+
+  if (!cell) {
+    printk(KERN_WARNING "rubyex: cell.remove: could not find cell named \"%s\"\n", token);
+    return;
+  }
+
+  cell_list_remove(rubyex_cells, cell);
 }
 
 char *get_next_nonblank_token(char **ssptr)
