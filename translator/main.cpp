213dffe100de2e0264aa6bee075c7c97bdedd686
Arlen Cuss <celtic@sohma>
2008-02-15 17:38:22 +1100

    Adding a basic.. start to a .. kinda testing framework. Sort of, but not really. We'll see.

--- translator/main.cpp
+++ translator/main.cpp
@@ -1,6 +1,28 @@
 #include "main.h"
+#include <list>
+#include <string>
+#include <cassert>
 
-int main()
+int main_parser(std::list<std::string> &);
+int main_test(std::list<std::string> &);
+
+int main(int argc, char **argv)
+{
+  std::string invoked_as = argv[0];
+  std::list<std::string> arguments;
+  for (int i = 1; i < argc; ++i)
+    arguments.push_back(argv[i]);
+
+  if (invoked_as.find('/') != std::string::npos)
+    invoked_as = invoked_as.substr(invoked_as.find('/') + 1);
+
+  if (invoked_as == "test")
+    return main_test(arguments);
+  
+  return main_parser(arguments);
+}
+
+int main_parser(std::list<std::string> &arguments)
 {
   Program p;
 
@@ -12,6 +34,14 @@ int main()
   p.p(0);
 
   return 0;
+}
+
+int main_test(std::list<std::string> &arguments)
+{
+  Program p;
+
+  int r = yyparse(&p);
+  assert(r == 0);
 }
 
 void yyerror(Program *p, char const *s)
