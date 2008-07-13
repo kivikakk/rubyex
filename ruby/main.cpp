774bff13957680fd24857f45bb13b70129eb42c8
Yuki Izumi <kivikakk@github.com>
2008-07-13 16:24:56 +1000

    The "ruby" package now actually processes Ruby, just like the original!

--- ruby/main.cpp
+++ ruby/main.cpp
@@ -1,5 +1,28 @@
+#include <sstream>
+#include <iostream>
+#include "vm/renvironment.h"
+#include "vm/process.h"
+#include "parser/ast.h"
+#include "parser/main.h"
+
+bool omit_errors = false; 	// required for parser.
+
 int main()
 {
+  std::ostringstream oss;
+  Program p(oss);
+  int r = yyparse(&p);
+  if (r != 0)
+    return r;
+
+  std::istringstream iss(oss.str());
+  RubyEnvironment e;
+  Reader reader(iss);
+  std::vector<Context *> cs;
+  Context *c = new Context(&e, RubyValue::from_object(e.main));
+
+  process(e, reader, cs, c);
+
   return 0;
 }
 
